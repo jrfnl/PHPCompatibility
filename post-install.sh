36b3a92c46c9351b1e1c93f73aa46d078d2ce7f9
jrfnl <github_nospam@adviesenzo.nl>
2016-09-14 19:14:31 +0200

    try again

--- post-install.sh
+++ post-install.sh
@@ -13,14 +13,26 @@ ls ./Sniffs -l
 
 #echo ""
 #echo "Clearing old install"
-#rm -rf ./PHPCompatibility/*
-#mkdir -p ./PHPCompatibility/Sniffs
+rm -rf ./PHPCompatibility/*
+mkdir -v PHPCompatibility
 
 #echo ""
 #echo "Moving new files"
-#mv -f Sniff.php ./PHPCompatibility/Sniff.php
-#mv -f Sniffs/* ./PHPCompatibility/Sniffs/*
+mv -f Sniff.php ./PHPCompatibility/Sniff.php
+mv -f Sniffs/* ./PHPCompatibility/Sniffs/*
 
 echo ""
 echo "Setting PHPCS installed_paths"
 "vendor/bin/phpcs" --config-set installed_paths ../../..
+
+echo "Check the result:"
+echo "File listings:"
+ls -l
+
+echo ""
+echo "PHPCompatibility Directory:"
+ls ./PHPCompatibility -l
+
+echo ""
+echo "Sniffs Directory:"
+ls ./Sniffs -l
