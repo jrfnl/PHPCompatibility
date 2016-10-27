4fadf94fc45f33798ba8faeebb09c89e93aba1ce
jrfnl <github_nospam@adviesenzo.nl>
2016-10-27 02:36:44 +0200

    All in one

---
+++ post-install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+echo "File listings:"
+ls -l
+
+echo ""
+echo "PHPCompatibility Directory:"
+ls ./PHPCompatibility -l
+
+echo ""
+echo "Sniffs Directory:"
+ls ./Sniffs -l
+
+#echo ""
+#echo "Clearing old install"
+rm -rf ./PHPCompatibility/*
+mkdir -v PHPCompatibility
+
+#echo ""
+#echo "Moving new files"
+mv -f Sniff.php ./PHPCompatibility/Sniff.php
+mv -f Sniffs/* ./PHPCompatibility/Sniffs/
+
+echo ""
+echo "Setting PHPCS installed_paths"
+"vendor/bin/phpcs" --config-set installed_paths ../../..
+
+echo "Check the result:"
+echo "File listings:"
+ls -l
+
+echo ""
+echo "PHPCompatibility Directory:"
+ls ./PHPCompatibility -l
+
+echo ""
+echo "Sniffs Directory:"
+ls ./Sniffs -l
